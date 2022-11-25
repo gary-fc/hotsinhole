5085536af62063435c136cdd979e8f4e302ae69b
Gary Ernesto <gary.franco.cespedes@gmail.com>
2022-11-25 01:42:07 -0400

    fix keyboard

--- resources/views/layaouts/auth.blade.php
+++ resources/views/layaouts/auth.blade.php
@@ -2,8 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, virtual-keyboard=overlays-content">
     <title>Hotsinhole - @yield('title')</title>
     <link rel="icon" type="image/x-icon" href="https://www.w3schools.com/favicon.ico">
 
