3bd78ff5d7cb54db198d230a75348a6e57be3d68
Gary Ernesto <gary.franco.cespedes@gmail.com>
2022-11-25 23:01:47 -0400

    fix register

--- resources/views/layaouts/auth.blade.php
+++ resources/views/layaouts/auth.blade.php
@@ -11,7 +11,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
-<body style="height: 100% !important;">
+<body>
 @include('shared.header_auth')
 
 @yield('content')
